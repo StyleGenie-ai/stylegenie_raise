51cb2aa29abf2b5314336d2ded8b40dc76e1f4ab
justxd22 <xdjust18@gmail.com>
2025-07-05 01:11:24 +0300

    Update the dataset using Nada json(s)

--- frontend/stylectai/pages/search.tsx
+++ frontend/stylectai/pages/search.tsx
@@ -33,7 +33,7 @@ const Index = () => {
     setHasSearched(true)
 
     try {
-      const response = await fetch("http://127.0.0.1:8800/api/query", {
+      const response = await fetch("http://127.0.0.1:8000/api/query", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
