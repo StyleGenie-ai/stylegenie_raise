39c2d913672d8c1968d36016bd2ecf9b443e6bba
justxd22 <xdjust18@gmail.com>
2025-07-07 06:24:40 +0300

    updated the prompt

--- frontend/stylectai/pages/search.tsx
+++ frontend/stylectai/pages/search.tsx
@@ -33,7 +33,7 @@ const Index = () => {
     setHasSearched(true)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/queryy", {
+      const response = await fetch("http://127.0.0.1:8000/api/queryy_men", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
